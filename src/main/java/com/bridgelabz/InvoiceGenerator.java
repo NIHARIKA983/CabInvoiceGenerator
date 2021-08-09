70c5a19ad36a03d747ab57512b15a8138693e4c3
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-08-09 22:07:05 +0530

    Added:Enhanced Invoice

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,9 +1,13 @@
 package com.bridgelabz;
 
+
+
 public class InvoiceGenerator {
+
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final double MINIMUM_FARE = 5;
+    private static final int numOfRide = 5;
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
@@ -16,10 +20,11 @@ public class InvoiceGenerator {
         return null;
     }
 
-    public double calculateFare(Ride[] rides) {
+    public InvoiceSummary calculateFare(Ride[] rides) {
         double totalfare = 0;
         for (Ride ride : rides) {
             totalfare += this.calculateFare(ride.distance, ride.time);
-        }return totalfare;
+        }
+        return new InvoiceSummary(rides.length, totalfare);
     }
 }
--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -17,7 +17,7 @@ public class InvoiceServiceTest {
         double distance = 2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(25, fare, 0.0);
+        Assert.assertEquals(25,fare,0.0);
     }
 
     @Test
@@ -25,18 +25,17 @@ public class InvoiceServiceTest {
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance, time);
-        Assert.assertEquals(5, fare, 0.0);
+        Assert.assertEquals(5,fare,0.0);
     }
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnTotalFare() {
         Ride[] rides = {new Ride(2.0, 5),
-                new Ride(0.1, 1)
+                new Ride(0.1,1)
         };
-        Object NRide[] = {invoiceGenerator.ride(2.0, 5),
-                invoiceGenerator.ride(0.1, 1)
-        };
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assert.assertEquals(30, fare, 0.0);
+        InvoiceSummary summary =  invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary,summary);
     }
+
 }
