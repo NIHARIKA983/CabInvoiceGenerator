c58b3c7eff5b1254938f53e60d821a791f52255c
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-08-10 12:35:04 +0530

    Added:To get the List of ride by user

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,13 +1,12 @@
 package com.bridgelabz;
 
-
-
 public class InvoiceGenerator {
 
     private static final int COST_PER_TIME = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
     private static final double MINIMUM_FARE = 5;
     private static final int numOfRide = 5;
+    RideRepository rideRepository = new RideRepository();
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
@@ -27,4 +26,27 @@ public class InvoiceGenerator {
         }
         return new InvoiceSummary(rides.length, totalfare);
     }
+
+    public InvoiceSummary calculateFare(Ride[] rides, String type) {
+        double totalFare = 0;
+        if (type == "premium") {
+            for (Ride ride : rides) {
+                totalFare += this.calculateFare(ride.distance, ride.time);
+            }
+            return new InvoiceSummary(rides.length, totalFare);
+        }
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public void addRides(String userId, Ride[] rides) {
+        rideRepository.addRide(userId, rides);
+    }
+
+    public InvoiceSummary getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId), "normal");
+    }
+
 }
